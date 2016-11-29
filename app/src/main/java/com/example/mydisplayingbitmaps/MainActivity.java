396eeb8fdd101e787c019f758d24d7ab16319fe2
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-11-29 19:40:04 +0900

    User AsyncTask

--- app/src/main/java/com/example/mydisplayingbitmaps/MainActivity.java
+++ app/src/main/java/com/example/mydisplayingbitmaps/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mydisplayingbitmaps;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,6 +13,7 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,11 +29,9 @@ public class MainActivity extends AppCompatActivity {
 
         final List<ImageView> listImageView = new ArrayList<>();
         for (int i = 0; i < 100 ; i++) {
-            Bitmap bm = decodeSampledBitmapFromResource(getResources(), R.drawable.sample, 180, 320);
-
             ImageView imageView = new ImageView(MainActivity.this);
             imageView.setLayoutParams(new GridView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 320));
-            imageView.setImageBitmap(bm);
+            loadBitmap(R.drawable.sample, imageView);
             listImageView.add(imageView);
 
             Runtime r = Runtime.getRuntime();
@@ -62,42 +62,75 @@ public class MainActivity extends AppCompatActivity {
         mGridView.setAdapter(mAdapter);
     }
 
-    public Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
-                                                         int reqWidth, int reqHeight) {
-
-        // First decode with inJustDecodeBounds=true to check dimensions
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeResource(res, resId, options);
-
-        // Calculate inSampleSize
-        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-        return BitmapFactory.decodeResource(res, resId, options);
+    public void loadBitmap(int resId, ImageView imageView) {
+        BitmapWorkerTask task = new BitmapWorkerTask(imageView, getResources());
+        task.execute(resId);
     }
 
-    public int calculateInSampleSize(
-            BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
+    class BitmapWorkerTask extends AsyncTask<Integer, Void, Bitmap> {
+        private final WeakReference<ImageView> imageViewReference;
+        private final Resources resources;
 
-        if (height > reqHeight || width > reqWidth) {
+        public BitmapWorkerTask(ImageView imageView, Resources resources) {
+            // Use a WeakReference to ensure the ImageView can be garbage collected
+            this.imageViewReference = new WeakReference<ImageView>(imageView);
+            this.resources = resources;
+        }
 
-            final int halfHeight = height / 2;
-            final int halfWidth = width / 2;
+        // Decode image in background.
+        @Override
+        protected Bitmap doInBackground(Integer... params) {
+            return decodeSampledBitmapFromResource(resources, params[0], 180, 320);
+        }
 
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-            // height and width larger than the requested height and width.
-            while ((halfHeight / inSampleSize) >= reqHeight
-                    && (halfWidth / inSampleSize) >= reqWidth) {
-                inSampleSize *= 2;
+        // Once complete, see if ImageView is still around and set bitmap.
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            if (imageViewReference != null && bitmap != null) {
+                final ImageView imageView = imageViewReference.get();
+                if (imageView != null) {
+                    imageView.setImageBitmap(bitmap);
+                }
             }
         }
 
-        return inSampleSize;
+        public Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                      int reqWidth, int reqHeight) {
+
+            // First decode with inJustDecodeBounds=true to check dimensions
+            final BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeResource(res, resId, options);
+
+            // Calculate inSampleSize
+            options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+            // Decode bitmap with inSampleSize set
+            options.inJustDecodeBounds = false;
+            return BitmapFactory.decodeResource(res, resId, options);
+        }
+
+        public int calculateInSampleSize(
+                BitmapFactory.Options options, int reqWidth, int reqHeight) {
+            // Raw height and width of image
+            final int height = options.outHeight;
+            final int width = options.outWidth;
+            int inSampleSize = 1;
+
+            if (height > reqHeight || width > reqWidth) {
+
+                final int halfHeight = height / 2;
+                final int halfWidth = width / 2;
+
+                // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+                // height and width larger than the requested height and width.
+                while ((halfHeight / inSampleSize) >= reqHeight
+                        && (halfWidth / inSampleSize) >= reqWidth) {
+                    inSampleSize *= 2;
+                }
+            }
+
+            return inSampleSize;
+        }
     }
 }
